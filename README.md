1fa1dcad8f4f3422afa9b993d33c31cbd337acc4
Nidalone29 <22301275+Nidalone29@users.noreply.github.com>
2023-06-23 20:59:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+###build status
+Windows:
+ - Works on clang
+ - with GCC (MinGW) there is an issue with compiling assimp
+ - it works with MSVC but the assets have to be moved manually because the location of the executable in the build directory differs from clang
---
+++ src/flower.cpp
@@ -0,0 +1,15 @@
+#include "flower.h"
+#include "common.h"
+
+Flower::Flower() : _initialized(false) {}
+
+void Flower::init(void) {
+  _model.load_mesh("models/flower/flower.obj", aiProcess_Triangulate);
+  _initialized = true;
+}
+
+void Flower::render(void) {
+  if (!_initialized) init();
+
+  _model.render();
+}
